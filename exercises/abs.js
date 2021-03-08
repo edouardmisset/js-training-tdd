b9d8cafd5d6c12064f05de36f8cffbb72116e73f
Edouard Misset <edouardmisset@gmail.com>
2021-03-08 19:48:35 +0100

    abs

--- exercises/abs.js
+++ exercises/abs.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create the `abs` function that takes one number argument
@@ -10,12 +10,15 @@
  */
 
 // Your code:
+function abs(params) {
+  return params < 0 ? -params : params;
+}
 
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 
-assert.strictEqual(typeof abs, 'function');
-assert.strictEqual(abs.toString().includes('Math.abs'), false);
+assert.strictEqual(typeof abs, "function");
+assert.strictEqual(abs.toString().includes("Math.abs"), false);
 assert.notStrictEqual(abs, Math.abs);
 assert.strictEqual(abs.length, 1);
 assert.strictEqual(abs(0), 0);
