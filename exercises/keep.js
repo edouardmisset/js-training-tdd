6b26cffb86cb918c06d8409f9aeb9c3d29512aa5
Edouard Misset <edouardmisset@gmail.com>
2021-03-09 12:42:12 +0100

    keep

--- exercises/keep.js
+++ exercises/keep.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create a `keepFirst` function that takes a string as parameter
@@ -13,9 +13,29 @@
  */
 
 // Your code:
+const keepFirst = (str) => str.slice(0, 2);
+
+const keepLast = (str) => str.slice(str.length - 2, str.length);
+
+const keepFirstLast = (str) =>
+  str.slice(0, 2) + str.slice(str.length - 2, str.length);
 
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof keepFirst, "function");
+assert.strictEqual(keepFirst.length, 1);
+assert.strictEqual(keepFirst("123456789"), "12");
+assert.strictEqual(keepFirst(""), "");
+
+assert.strictEqual(typeof keepLast, "function");
+assert.strictEqual(keepLast.length, 1);
+assert.strictEqual(keepLast("123456789"), "89");
+assert.strictEqual(keepLast(""), "");
+
+assert.strictEqual(typeof keepFirstLast, "function");
+assert.strictEqual(keepFirstLast.length, 1);
+assert.strictEqual(keepFirstLast("123456789"), "1289");
+assert.strictEqual(keepFirstLast(""), "");
+// assert.fail('You must write your own tests');
 // End of tests */
