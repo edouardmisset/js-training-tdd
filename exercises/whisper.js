86d1efd6205866b7cfb52b2620143a3ff734420b
Edouard Misset <edouardmisset@gmail.com>
2021-03-09 12:20:05 +0100

    corrected yell and whisper

--- exercises/whisper.js
+++ exercises/whisper.js
@@ -16,8 +16,8 @@ const assert = require("assert");
 
 assert.strictEqual(typeof yell, "function");
 assert.strictEqual(yell.length, 1);
-assert.deepStrictEqual(yell("Hello"), "*hello*");
-assert.deepStrictEqual(yell("Hello, WORLD!"), "*hello, world!*");
+assert.strictEqual(yell("Hello"), "*hello*");
+assert.strictEqual(yell("Hello, WORLD!"), "*hello, world!*");
 
 // assert.fail('You must write your own tests');
 // End of tests */
--- exercises/yell.js
+++ exercises/yell.js
@@ -15,7 +15,7 @@ const assert = require("assert");
 
 assert.strictEqual(typeof yell, "function");
 assert.strictEqual(yell.length, 1);
-assert.deepStrictEqual(yell("Hello"), "HELLO");
-assert.deepStrictEqual(yell("Hello, World!"), "HELLO, WORLD!");
+assert.strictEqual(yell("Hello"), "HELLO");
+assert.strictEqual(yell("Hello, World!"), "HELLO, WORLD!");
 // assert.fail("You must write your own tests");
 // End of tests */
