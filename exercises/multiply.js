8cd76b7e61681997cf3f039b2fd51b814ffe06fb
Edouard Misset <edouardmisset@gmail.com>
2021-03-09 16:20:36 +0100

    multiply

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create a function `multiply` that takes two number arguments
@@ -10,19 +10,30 @@
 
 // Your code:
 
-//* Begin of tests
-const assert = require('assert');
+const multiply = (a, b) => {
+  let result = 0;
+  for (let index = 0; index < Math.abs(b); index++) {
+    result += Math.abs(a);
+  }
 
-assert.strictEqual(typeof multiply, 'function');
+  return (a > 0 && b > 0) || (a < 0 && b < 0) || a === 0 || b === 0
+    ? result
+    : -result;
+};
+
+//* Begin of tests
+const assert = require("assert");
+
+assert.strictEqual(typeof multiply, "function");
 assert.strictEqual(multiply.length, 2);
-assert.strictEqual(multiply.toString().includes('Math.imul'), false);
-assert.strictEqual(multiply.toString().includes('*'), false);
-assert.strictEqual(multiply.toString().includes('/'), false);
+assert.strictEqual(multiply.toString().includes("Math.imul"), false);
+assert.strictEqual(multiply.toString().includes("*"), false);
+assert.strictEqual(multiply.toString().includes("/"), false);
 assert.strictEqual(multiply(34, 78), 2652);
 assert.strictEqual(multiply(123, 0), 0);
 assert.strictEqual(multiply(0, -230), 0);
 assert.strictEqual(multiply(0, 0), 0);
+assert.strictEqual(multiply(-22, -123), 2706);
 assert.strictEqual(multiply(123, -22), -2706);
 assert.strictEqual(multiply(-22, 123), -2706);
-assert.strictEqual(multiply(-22, -123), 2706);
 // End of tests */
