5fb441abf15f803830dd428ed083061055910b68
Edouard Misset <edouardmisset@gmail.com>
2021-03-09 12:35:13 +0100

    cut

--- exercises/cut.js
+++ exercises/cut.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * Create the `cutFirst` function that takes a string and remove the 2 first characters
@@ -11,9 +11,29 @@
  */
 
 // Your code:
+const cutFirst = (str) => str.slice(2);
+
+const cutLast = (str) => str.slice(0, str.length - 2);
+
+const cutFirstLast = (str) => str.slice(2, str.length - 2);
 
 //* Begin of tests
-const assert = require('assert');
+const assert = require("assert");
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof cutFirst, "function");
+assert.strictEqual(cutFirst.length, 1);
+assert.strictEqual(cutFirst("123456789"), "3456789");
+assert.strictEqual(cutFirst(""), "");
+
+assert.strictEqual(typeof cutLast, "function");
+assert.strictEqual(cutLast.length, 1);
+assert.strictEqual(cutLast("123456789"), "1234567");
+assert.strictEqual(cutLast(""), "");
+
+assert.strictEqual(typeof cutFirstLast, "function");
+assert.strictEqual(cutFirstLast.length, 1);
+assert.strictEqual(cutFirstLast("123456789"), "34567");
+assert.strictEqual(cutFirstLast(""), "");
+
+// assert.fail('You must write your own tests');
 // End of tests */
